d8c9056e6de182f91c73e434fba3c357a8e1e0b4
Jay Gudsson <gudsson@gmail.com>
2021-07-05 16:11:33 -0700

    contact list template implemented

---
+++ public/javascripts/main.js
@@ -0,0 +1,32 @@
+$(() => {
+  let $container = $('[style]');
+
+  let rowWell = Handlebars.compile($('#rowWell').html());
+  Handlebars.registerPartial('rowWell', $('#rowWell').html());
+
+  // alert('loaded');
+  let contacts = [{
+    id: 1,
+    full_name: 'Jay Gudsson',
+    email: 'gudsson@gmail.com',
+    phone_number: '6043519761',
+    // tags: []
+  }, {
+    id: 2,
+    full_name: 'Philomena Chenne',
+    email: 'phichenne@yahoo.ca',
+    phone_number: '7783956655',
+    // tags: []
+  }]
+
+  let contactsList = Handlebars.compile($('#contactsList').html());
+  let contactTemplate = Handlebars.compile($('#contactTemplate').html());
+  
+
+  Handlebars.registerPartial('contactTemplate', $('#contactTemplate').html());
+
+  // $list.html(contactsList({ contacts: contacts }));
+
+  // $($container).append($list);
+  $container.html(contactsList({ contacts: contacts }))
+});
